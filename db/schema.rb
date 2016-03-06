664a5b0659e5de1feafd8fb79a712992df72fe53
cottoncandyybk <claudiayuki.shino@gmail.com>
2016-03-06 02:36:53 +0000

    Kadai

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228143507) do
+ActiveRecord::Schema.define(version: 20160305070519) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20160228143507) do
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
   add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
+
+  create_table "r_elationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "r_elationships", ["followed_id"], name: "index_r_elationships_on_followed_id"
+  add_index "r_elationships", ["follower_id"], name: "index_r_elationships_on_follower_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
